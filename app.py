af97ed01ee48071b9b9781c211fddccdbce863db
emilianospinoso <emilianospinoso@gmail.com>
2021-07-13 11:46:15 -0300

    crudSinFoto

--- app.py
+++ app.py
@@ -44,6 +44,21 @@ def edit(id):
     return render_template('empleados/edit.html', empleados=empleados)
 
 
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _foto=request.files['txtFoto']
+    id=request.form['txtID']
+
+    sql="UPDATE empleados SET nombre=%s, correo=%s WHERE id=%s;"
+    datos=(_nombre, _correo,id)
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/')
+
 @app.route('/create')
 def create():
    return render_template('empleados/create.html')
