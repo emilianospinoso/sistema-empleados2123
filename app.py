dc3db0a110da2747fd1e768ed344718140048616
emilianospinoso <emilianospinoso@gmail.com>
2021-07-13 10:39:44 -0300

    insercion sin fotola

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template , request
 from flaskext.mysql import MySQL
 
 app= Flask(__name__)
@@ -15,13 +15,35 @@ mysql.init_app(app)
 
 @app.route('/')
 def index():
-    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'laura', 'obama@ciudad.com.ar', 'dios.jpg');"
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'messi', 'messi@ciudad.com.ar', 'messi.jpg');"
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
     conn.commit()
     
     return render_template('empleados/index.html')
+
+
+@app.route('/create')
+def create():
+   return render_template('empleados/create.html')
+
+
+@app.route('/store', methods=['POST'])
+def storage():
+   _nombre=request.form['txtNombre']
+   _correo=request.form['txtCorreo']
+   _foto=request.files['txtFoto']
+
+   sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL,%s,%s,%s);"
+   datos=(_nombre, _correo , _foto.filename)
+
+   conn=mysql.connect()
+   cursor=conn.cursor()
+   cursor.execute(sql, datos)
+   conn.commit()
+   return render_template('empleados/index.html')
+
 
 
 if __name__=='__main__':
